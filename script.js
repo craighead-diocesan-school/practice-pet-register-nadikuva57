7e19972f739134de3d9ae05ab957398e288b749e
Varuni <nadikuva57@craighead.school.nz>
2023-07-24 19:32:20 +1200

    Added removed name function

--- script.js
+++ script.js
@@ -6,5 +6,14 @@ let animalName = [
 
 function addName() {
     let newName = prompt("What name do you want to add? ")
-    name.push(newName)
-  }
+    animalName.push(newName)
+}
+
+function removeName() {
+    let index = prompt("choose a number between 0 and 2")
+    animalName.splice(index, 1)
+}
+
+
+
+  
