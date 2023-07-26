8f80f7892a67f97fa6594dfd3dd7be962ee6b4e7
Varuni <nadikuva57@craighead.school.nz>
2023-07-26 14:50:02 +1200

    Fixed up the functon on the index. Changed the list to include age and species. which i also added to the name function.

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
-let animalName = [
-    'Scruffy',
-    'Cleo',
-    'Zuri',
+let animal = [
+    {name: 'Blinky', age: 5, species:'Sheep'},
+    {name: 'Zuri', age: 8, species:'Dog'},
+    {name: 'Chunky', age: 3, species:'Rabbit'},
 ]
 
-function addName() {
-    let newName = prompt("What name do you want to add? ")
-    animalName.push(newName)
+function addPet() {
+    let newName = prompt('What name do you want to add?')
+    let newAge = prompt('What is the age of your pet?')
+    let newSpicies = prompt('What is the spieces of your pet?')
+    let pet = {
+        newName: newName,
+        newAge: newAge,
+        newSpicies: newSpicies
+    }
+    animal.push(pet)
 }
 
 function showPets() {
-    alert(animalName)
+    alert(animal)
 }
 
 function removeName() {
     let index = prompt("choose a number between 0 and 2")
-    animalName.splice(index, 1)
+    animal.splice(index, 1)
 }
 
 
